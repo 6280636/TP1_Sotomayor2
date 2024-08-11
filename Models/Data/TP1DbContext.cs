3f90f8c7c2d57ec4f63dbe46211dd2e99d6430cb
victor <6280636@cegepmontpetit.ca>
2024-08-11 17:58:03 -0400

    FCT Configuration EntityFramwork DbContext DI
    Création du ApplicationDbContext et ajout de DI du service SQL Server et de la connexion à la BD.

---
+++ Models/Data/TP1DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TP1_Sotomayor.Models.Data
+{
+    public class TP1DbContext : DbContext
+    {
+        public TP1DbContext(DbContextOptions<TP1DbContext> options) : base(options)
+        {
+
+        }
+
+    }
+
+
+}
